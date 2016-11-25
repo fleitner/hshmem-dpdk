379398d15635cb45d38882047a0c36e702a9adb9
Flavio Leitner <fbl@redhat.com>
2016-11-25 18:32:45 -0200

    [pmd] fix wrong device status.
    Signed-off-by: Flavio Leitner <fbl@redhat.com>

--- drivers/net/hshmem/pmd_hshmem.c
+++ drivers/net/hshmem/pmd_hshmem.c
@@ -378,7 +378,7 @@ hshmem_recv_pkts(void *rx_queue, struct rte_mbuf **rx_pkts, uint16_t nb_pkts)
 	uint16_t ndq;
 	uint16_t cnt;
 
-	if (!adapter->stopped)
+	if (adapter->stopped)
 		return 0;
 
 	if (nb_pkts > HSHMEM_MAX_BURST)
